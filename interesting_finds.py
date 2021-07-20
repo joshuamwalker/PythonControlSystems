40ea20d62d9f75bd302ba4da04469641cbe40b60
joshuamwalker <44822615+joshuamwalker@users.noreply.github.com>
2021-07-19 19:13:42 -0500

    Tidied up TD, FD, Polyfit visualization.

--- interesting_finds.py
+++ interesting_finds.py
@@ -1,42 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from plotting import plot_TD_FD, poly_fit_plot
+import math
 
 
-def polyfit_whitenoise_vs_randomwalk():
+def polyfit_exploration():
     # Found that integrating a polyfit on white noise
     # produces a very similar result to performing the polyfit
     # on the integrated white noise (random walk)
+
     n = 100000  # Number of elements
+    nf = int(n/2+1)  # Number of elements in FD
     fs = 1
     t = np.linspace(0, n/fs, n, False)  # Do not include endpoint
+    f = np.linspace(0, fs/2, nf, True)  # Include endpoint. Number of elements is n/2 + 1
 
-    whiteNoise = np.random.rand(n) - 0.5
+    # Compute Time Domain WN, RW, and their polyfits
+    wn_td = np.random.rand(n) - 0.5  # White Noise, Time Domain
+    rw_td = np.cumsum(wn_td)  # Random Walk, Time Domain
+    wd_td_pf_td = poly_fit_plot(t, wn_td, 20, n)[1][1]
+    rw_td_pf_td = poly_fit_plot(t, rw_td, 20, n)[1][1]
 
-    randomWalk = np.zeros(n)
-    for idx, val in enumerate(randomWalk):
-        randomWalk[idx] = whiteNoise[0] if idx == 0 else randomWalk[idx - 1] + whiteNoise[idx]
+    # Take DFT of each (real) signal
+    wn_fd = abs(np.fft.rfft(wn_td))
+    rw_fd = abs(np.fft.rfft(rw_td))
+    wd_td_pf_fd = abs(np.fft.rfft(wd_td_pf_td))
+    rw_td_pf_fd = abs(np.fft.rfft(rw_td_pf_td))
 
-    gaussNoise = np.random.randn(n)
+    # Perform polyfit on Frequency Domain signals
+    wd_fd_pf_fd = poly_fit_plot(f, wn_fd, 20, nf)[1][1]
+    rw_fd_pf_fd = poly_fit_plot(f, rw_fd, 20, nf)[1][1]
+    f_ls = np.rint(10**np.linspace(0, math.log10(n/2), int(n/10), False)).astype(int)  # Generate sampling indices for logarithmic sampling
+    wd_fd_ls_pf_fd = poly_fit_plot(f[[f_ls]], wn_fd[[f_ls]], 20, nf)[1]
+    rw_fd_ls_pf_fd = poly_fit_plot(f[[f_ls]], rw_fd[[f_ls]], 20, nf)[1]
+    # wd_fd_ls_pf_fd = np.polynomial.Polynomial.fit(x, y, order).linspace(n, [0, max(t)])
+    # rw_fd_ls_pf_fd =
 
-    gaussWalk = np.zeros(n)
-    for idx, val in enumerate(gaussWalk):
-        gaussWalk[idx] = gaussNoise[0] if idx == 0 else gaussWalk[idx - 1] + gaussNoise[idx]
+    # Take IDFT of each polyfit frequency data
+    # data = np.fft.irfft(Data)
 
     fig = plt.figure(figsize=(12, 6))
-    sz = np.array([2, 2])
-    whiteNoiseAx = plot_TD_FD(whiteNoise, fs=1, timeAx=fig.add_subplot(sz[0], sz[1], 1), freqAx=fig.add_subplot(sz[0], sz[1], 3), tLim=np.array([0, n]), title="White Noise")
-    randomWalkAx = plot_TD_FD(randomWalk, fs=1, timeAx=fig.add_subplot(sz[0], sz[1], 2), freqAx=fig.add_subplot(sz[0], sz[1], 4), tLim=np.array([0, n]), title="Random Walk")
+    wn_td_ax = fig.add_subplot(221, title='White Noise, TD')
+    rw_td_ax = fig.add_subplot(222, title='Random Walk, TD')
+    wn_fd_ax = fig.add_subplot(223, title='White Noise, FD', xscale='log', yscale='log')
+    rw_fd_ax = fig.add_subplot(224, title='Random Walk, FD', xscale='log', yscale='log')
 
-    whiteNoise_polyfit,  whiteNoise_polypts = poly_fit_plot(t, whiteNoise, 20, n, axes=whiteNoiseAx[0], color='magenta')
-    whiteNoise_polyfit_integrated = np.zeros(n)
-    for idx, val in enumerate(whiteNoise_polypts[0]):
-        whiteNoise_polyfit_integrated[idx] = whiteNoise_polypts[1][0] if idx == 0 else whiteNoise_polyfit_integrated[idx - 1] + whiteNoise_polypts[1][idx]
-    randomWalkAx[0].plot(t, whiteNoise_polyfit_integrated, color='magenta')
+    wn_td_ax.plot(t, wn_td, color='black')
+    rw_td_ax.plot(t, rw_td, color='black')
+    wn_fd_ax.plot(f, wn_fd, color='black')
+    rw_fd_ax.plot(f, rw_fd, color='black')
 
-    poly,  polypts = poly_fit_plot(t, randomWalk, 20, n, axes=randomWalkAx[0], color='blue')
+    wn_td_ax.plot(t, wd_td_pf_td, color='blue')  # Blue indicates polyfit perfomed in TD
+    rw_td_ax.plot(t, rw_td_pf_td, color='blue')
 
-    whiteNoiseAx = plot_TD_FD(whiteNoise_polypts[1], fs=1, timeAx=whiteNoiseAx[0], freqAx=whiteNoiseAx[1], title="White Noise", tColor='magenta', fColor='magenta')
+    wn_fd_ax.plot(f, wd_td_pf_fd, color='blue')
+    rw_fd_ax.plot(f, rw_td_pf_fd, color='blue')
+    # wn_fd_ax.plot(f, wd_fd_pf_fd, color='magenta', linestyle='dashed')  # Magenta indicates polyfit perfomed in FD
+    # rw_fd_ax.plot(f, rw_fd_pf_fd, color='magenta', linestyle='dashed')
+    # wn_fd_ax.plot(wd_fd_ls_pf_fd[0], wd_fd_ls_pf_fd[1], color='magenta')
+    # rw_fd_ax.plot(rw_fd_ls_pf_fd[0], rw_fd_ls_pf_fd[1], color='magenta')
+
+
+    # noise_1f = np.mean(np.divide(abs(Data[1:]), f[1:]))/f[1:]
+    # freqAx.plot(f[1:], noise_1f, color='red')
+
+    # whiteNoise_polyfit,  whiteNoise_polypts = poly_fit_plot(t, whiteNoise, 20, n, axes=whiteNoiseAx[0], color='magenta')
+    # whiteNoise_polyfit_integrated = np.cumsum(whiteNoise_polypts[1])
+    # randomWalkAx[0].plot(t, whiteNoise_polyfit_integrated, color='magenta')
+
+
     # randomWalkAx = plot_TD_FD(randomWalk, fs=1, timeAx=randomWalkAx[0], freqAx=randomWalkAx[1], title="Random Walk")
 
     plt.show()
@@ -44,4 +76,4 @@ def polyfit_whitenoise_vs_randomwalk():
     return()
 
 
-polyfit_whitenoise_vs_randomwalk()
+polyfit_exploration()
--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from random import seed
 from random import random
@@ -17,8 +18,8 @@ from random import random
     # Let y = Ap, where A = [[... x^2 x 1]] and p = [[c_2], [c_1], [c_0]]
     # So for y=mx+b, A = [[x 1]] and p = [[m], [b]]
 
-x = np.array([0, 1, 2, 3, 4, 5, 6])
-y = np.array([-1, 0.2, 0.9, 2.1, 4, 6, 10])
+# x = np.array([0, 1, 2, 3, 4, 5, 6])
+# y = np.array([-1, 0.2, 0.9, 2.1, 4, 6, 10])
 # A = np.vstack([x, np.ones(len(x))]).T
 # print(A)
 #
@@ -30,5 +31,18 @@ y = np.array([-1, 0.2, 0.9, 2.1, 4, 6, 10])
 # _ = plt.legend()
 # plt.show()
 
-tes = np.linspace(3, 8, 20, False)
-print(tes)
+# n = 1000
+# x = np.linspace(0, n, n, False)
+# wn_td = np.random.rand(n) - 0.5  # White Noise, Time Domain
+# y = np.cumsum(wn_td)  # Random Walk, Time Domain
+# plt.plot(x, y)
+# for order in [2, 4, 8, 16]:
+#     poly = np.polynomial.Polynomial.fit(x, y, order).linspace(n, [min(x), max(x)])
+#     plt.plot(poly[0], poly[1])
+#
+# plt.show()
+
+A = [5., 3, 7.2]
+print(A)
+B = np.int(A)
+print(B)
